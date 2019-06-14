64e321dc6f6534e088af2cfb14731b0995cb2779
leesan123 <s.lee@rescuenow.co.jp>
2019-06-14 16:54:43 +0900

    first commit

---
+++ example/create_dummy_log.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+touch -d "1 days ago" rsync_20190613.log
+touch -d "2 days ago" rsync_20190612.log
+touch -d "3 days ago" rsync_20190611.log
+touch -d "4 days ago" rsync_20190610.log
+touch -d "5 days ago" rsync_20190609.log
+touch -d "6 days ago" rsync_20190608.log
+touch -d "7 days ago" rsync_20190607.log
+touch -d "8 days ago" rsync_20190606.log
+touch -d "9 days ago" rsync_20190605.log
+touch -d "10 days ago" rsync_20190604.log
+touch -d "11 days ago" rsync_20190603.log
+touch -d "12 days ago" rsync_20190602.log
+touch -d "13 days ago" rsync_20190601.log
