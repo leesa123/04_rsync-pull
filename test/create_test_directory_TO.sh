6637013f3c35f0e0f6bea14ff248ea37cce78890
root <root@zabbix-agent-mysql.asia-northeast1-a.c.rescuepj-utapp-9898.internal>
2019-06-18 17:25:02 +0900

    first commit

--- test/create_test_directory_TO.sh
+++ test/create_test_directory_TO.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-if [ -e /opt/rsync_pull/log ]; then
+if [ -e /opt/rsync-pull/log ]; then
         echo "" > /dev/null
 else
-        mkdir /opt/rsync_pull/log
+        mkdir /opt/rsync-pull/log
 fi
-touch -d "1 days ago" /opt/rsync_pull/log/rsync_20190613.log
-touch -d "2 days ago" /opt/rsync_pull/log/rsync_20190612.log
-touch -d "3 days ago" /opt/rsync_pull/log/rsync_20190611.log
-touch -d "4 days ago" /opt/rsync_pull/log/rsync_20190610.log
-touch -d "5 days ago" /opt/rsync_pull/log/rsync_20190609.log
-touch -d "6 days ago" /opt/rsync_pull/log/rsync_20190608.log
-touch -d "7 days ago" /opt/rsync_pull/log/rsync_20190607.log
-touch -d "8 days ago" /opt/rsync_pull/log/rsync_20190606.log
-touch -d "9 days ago" /opt/rsync_pull/log/rsync_20190605.log
-touch -d "10 days ago" /opt/rsync_pull/log/rsync_20190604.log
-touch -d "11 days ago" /opt/rsync_pull/log/rsync_20190603.log
-touch -d "12 days ago" /opt/rsync_pull/log/rsync_20190602.log
-touch -d "13 days ago" /opt/rsync_pull/log/rsync_20190601.log
+touch -d "1 days ago" /opt/rsync-pull/log/rsync_20190613.log
+touch -d "2 days ago" /opt/rsync-pull/log/rsync_20190612.log
+touch -d "3 days ago" /opt/rsync-pull/log/rsync_20190611.log
+touch -d "4 days ago" /opt/rsync-pull/log/rsync_20190610.log
+touch -d "5 days ago" /opt/rsync-pull/log/rsync_20190609.log
+touch -d "6 days ago" /opt/rsync-pull/log/rsync_20190608.log
+touch -d "7 days ago" /opt/rsync-pull/log/rsync_20190607.log
+touch -d "8 days ago" /opt/rsync-pull/log/rsync_20190606.log
+touch -d "9 days ago" /opt/rsync-pull/log/rsync_20190605.log
+touch -d "10 days ago" /opt/rsync-pull/log/rsync_20190604.log
+touch -d "11 days ago" /opt/rsync-pull/log/rsync_20190603.log
+touch -d "12 days ago" /opt/rsync-pull/log/rsync_20190602.log
+touch -d "13 days ago" /opt/rsync-pull/log/rsync_20190601.log
 
 # create resource directory
 if [ -e /var/www/html ]; then
