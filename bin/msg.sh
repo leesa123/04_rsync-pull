ac12f4d43052308409d1b8d2343f43cd58456ef8
leesa123 <48462749+leesa123@users.noreply.github.com>
2019-07-01 14:08:54 +0900

    Update msg.sh

--- bin/msg.sh
+++ bin/msg.sh
@@ -31,7 +31,7 @@ exit 1
 blank_error() {
 cat <<_EOT_
 Error:
-        The variable _SRC_DIR in the cmd-variables.sh contains a space or tab character
+        Variables _SRC_DIR, _DEST_DIR in the cmd-variables.sh contains a space or tab character
 
 Risk:
         Mistakes result in unintended results being copied. This can cause fatal errors on Linux OS system.
