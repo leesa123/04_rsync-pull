6951e5631fd8fc832d47ebe1a0aac1f0a65e8ddf
root <root@zabbix-agent-mysql.asia-northeast1-a.c.rescuepj-utapp-9898.internal>
2019-07-01 14:04:46 +0900

    1

--- bin/cmd-variables.sh
+++ bin/cmd-variables.sh
@@ -2,20 +2,20 @@
 . ./msg.sh
 
 # Production server's ip
-_SRC_IP=10.146.0.13
+_SRC_IP=xxxx
 
 # !Caution!
 # In the case of a directory, add '/' of literal to the end of the path.
 # ex) '/var/www/html/'
 
 # Production server's symmetric directory for synchronization
-_SRC_DIR=/tmp/rsync_test/html/ 
+_SRC_DIR=xxxx
 if [ $? -ne 0 ]; then
 	blank_error
 	exit 1
 fi
 # Backup server's symmetric directory for synchronization
-_DEST_DIR='/tmp/rsync_test/html/'
+_DEST_DIR=xxxx
 if [ $? -ne 0 ]; then
 	blank_error
 	exit 1
